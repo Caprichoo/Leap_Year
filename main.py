1ba35a0040071af7421492616ddacd021a72b776
Baghel <sandeep.baghel@capgemini.com>
2024-02-24 19:52:07 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Which year do you want to check?
+year = int(input())
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+if year%4==0:
+  print("Leap year")
+    if year%100==0:
+        print("Leap year")
+    else year % 400 == 0:
+        print("Leap year")
+else:
+print("Not leap year")
+
